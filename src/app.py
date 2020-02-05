c41b22affab33326d25280aeee1267e1192d7fca
Jianing Lu <jl7003@nyu.edu>
2020-02-04 21:44:22 -0500

    add description

--- src/app.py
+++ src/app.py
@@ -17,6 +17,8 @@ import sys
 from flask import Flask, jsonify, request, flash, redirect, url_for, render_template, send_from_directory
 
 app = Flask(__name__)
+
+# Define dirs and requirements
 total_brands = ['Louis Vuitton', 'Chanel', 'Coach', 'Michael Kors', 'Gucci', 'Longchamp', 'Other', 'None']
 brands = [total_brands[i] for i in [1,2,0,6,1,5,4,4,1,3,3,0,0,7,0,3,4,0,4]]
 names = ['2.55','F57842', 'alma','handbag','boy','classic','dionysus','ggmarmont','grandshoppingtote','jetsettote','mercertotebag',
@@ -46,7 +48,23 @@ UPLOAD_FOLDER = '../data/example'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 ALLOWED_EXTENSIONS = { 'png', 'jpg', 'jpeg'}
 tf.gfile = tf.io.gfile
+
+# Model loading and image preprocessing (crop)
 def load_detection_model(model_name):
+    """
+    Load detection model from tensorflow
+    
+    Parameters
+    ----------
+    model_name : str
+        model name that will be loaded, can be found in 
+        /Users/jianinglu1/Documents/GitHub/models/research/object_detection/g3doc/detection_model_zoo.md
+
+    Returns
+    -------
+    model
+        detection model
+    """
     base_url = 'http://download.tensorflow.org/models/object_detection/'
     model_file = model_name + '.tar.gz'
     model_dir = tf.keras.utils.get_file(
@@ -62,6 +80,16 @@ def load_detection_model(model_name):
     return model
 
 def load_model():
+    """
+    Load the identification model for handbags
+    
+    Returns
+    -------
+    model_ft
+        whole model for predict brand and type
+    model_test
+        model for get the handbag vector (remove the last layer of whole model)
+    """
     model_ft = models.resnet50(pretrained=True)
     num_ftrs = model_ft.fc.in_features
     # Here the size of each output sample is set to 2.
@@ -77,8 +105,22 @@ def load_model():
     model_test.eval()
     return model_ft, model_test
 
-
 def run_inference_for_single_image(model, image):
+    """
+    Get object detection results
+    
+    Parameters
+    ----------
+    model : object
+        detection model
+    image : image object
+        input image
+    
+    Returns
+    -------
+    dictionary
+        object detection results
+    """
     image = np.asarray(image)
     # The input needs to be a tensor, convert it using `tf.convert_to_tensor`.
     input_tensor = tf.convert_to_tensor(image)
@@ -98,20 +140,25 @@ def run_inference_for_single_image(model, image):
 
     # detection_classes should be ints.
     output_dict['detection_classes'] = output_dict['detection_classes'].astype(np.int64)
-   
-    # Handle models with masks:
-    #if 'detection_masks' in output_dict:
-        # Reframe the the bbox mask to the image size.
-    #    detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(
-    #          output_dict['detection_masks'], output_dict['detection_boxes'],
-    #           image.shape[0], image.shape[1])      
-    #    detection_masks_reframed = tf.cast(detection_masks_reframed > 0.5,
-    #                                   tf.uint8)
-    #    output_dict['detection_masks_reframed'] = detection_masks_reframed.numpy()
-    
+
     return output_dict
 
 def crop_image(image_bytes, model):
+    """
+    Crop image based on object detection results and only keep the handbag part of image
+    
+    Parameters
+    ----------
+    image_bytes : input image, read from input image file
+        input image
+    model : object
+        detection model
+    
+    Returns
+    -------
+    img
+        crop img if handbag detection is successfult, None if no handbag has been detected.
+    """
     image = Image.open(io.BytesIO(image_bytes))
     width, hight = image.size
     image_np = np.array(image)
@@ -126,19 +173,41 @@ def crop_image(image_bytes, model):
     else:
         return False
 
-model, model_vector = load_model()
-
-model_name = 'faster_rcnn_inception_v2_coco_2018_01_28'
-detection_model = load_detection_model(model_name)
-
-
 def transform_image(image):
+    """
+    Transform image to get the input for identification model
+    
+    Parameters
+    ----------
+    image : image object
+        input image
+    
+    Returns
+    -------
+    transformed image
+    """
     my_transforms = transforms.Compose([transforms.Resize((256,256)),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
     return my_transforms(image).unsqueeze(0)
 
 def get_prediction(image, model, model_vector):
+    """
+    Get identification results
+    
+    Parameters
+    ----------
+    image : image object
+        transformed image
+    model : object
+        identification model
+    model_vector : object
+        model used to get handbag vector
+    
+    Returns
+    -------
+    prediction results
+    """
     tensor = transform_image(image=image)
     outputs = model(tensor)
     vectors = model_vector(tensor).detach().numpy()
@@ -149,17 +218,38 @@ def get_prediction(image, model, model_vector):
     return brands[predicted_idx], names[predicted_idx], websites[predicted_idx], vectors
 
 
-### recommendation part
+# Load identification model 
+model, model_vector = load_model()
+# Load detection model
+# faster rcnn trained using coco 
+model_name = 'faster_rcnn_inception_v2_coco_2018_01_28'
+detection_model = load_detection_model(model_name)
 
+# Handbag recommendatation 
+# read handbag information data
 shop_data_shein = pd.read_csv("../data/shop/file/shop_shein_index_final.csv")
 shop_data_saks = pd.read_csv("../data/shop/file/shop_bag_index_final.csv")
 vectors_shop = torch.load("../data/shop/file/shop_bag_vect_reorder.pt")
 vectors_shein = torch.load("../data/shop/file/shop_shein_bag_vect_reorder.pt")
-
 vectors_shop = np.reshape(vectors_shop,[2344,2048])
 vectors_shein = np.reshape(vectors_shein,[1270,2048])
 
 def recommend_bag_sim(input_vectors, vectors_bags):
+    """
+    Calculate similarity of handbags and rank handbags
+    
+    Parameters
+    ----------
+    input_vectors : numpy array
+        input handbag vector
+    vectors_bags : numpy array
+        retailer handbags vector
+    
+    Returns
+    -------
+    sim_data
+        data frame with data ranked by similarity
+    """
     sim = cosine_similarity(input_vectors, vectors_bags)
     sim = np.reshape(sim, np.shape(sim)[1])
     sim_dict = [["sim_" + str(i),j] for (i,j) in zip(range(len(sim)), sim)]
@@ -170,8 +260,31 @@ def recommend_bag_sim(input_vectors, vectors_bags):
     sim_data.columns = ["initial_index", "sim", "index", "order"]
     return sim_data
 
-
-def recommend_bag(input_vector, shop_data_saks, shop_data_shein, price_range, vetors_shein, vectors_shop, company):
+def recommend_bag(input_vector, shop_data_saks, shop_data_shein, price_range, vetors_shein, vectors_saks, company):
+    """
+    Recommend Top3 handbags based criteria
+    
+    Parameters
+    ----------
+    input_vector : numpy array
+        input handbag vector
+    shop_data_saks : data frame
+        saks data set
+    shop_data_shein : data frame
+        shein data set
+    price_range : str
+        price range required by user
+    vetors_shein : numpy array
+        handbag vectors from shein
+    vectors_shop : numpy array
+        handbag vectors from saks
+    company : str
+        retailer required by user
+    
+    Returns
+    -------
+    Top3 recommended handbags
+    """
     price_dict = {"less 50": ["less 50"],
                  "less 250" : ["less 50", "less 250"],
                  "less 500" : ["less 50", "less 250", "less 500"],
@@ -210,10 +323,24 @@ def recommend_bag(input_vector, shop_data_saks, shop_data_shein, price_range, ve
     return Top_1, Top_2, Top_3
 
 def get_infor(shop_data, index):
+    """
+    Get the information of data
+    
+    Parameters
+    ----------
+    shop_data : data frame
+        handbag data set
+    index : int
+        index of rows
+    
+    Returns
+    -------
+    handbag information
+    """
     shop_data = shop_data[index:index+1]
     return [shop_data["page"].values[0], shop_data["product"].values[0], shop_data["brand"].values[0], shop_data["price"].values[0], shop_data["image"].values[0], round(shop_data["sim"].values[0],2)]
 
-
+# Build web app 
 @app.route("/")
 def hello():
     return render_template('home.html')
@@ -238,12 +365,14 @@ def predict():
         else:
             file = request.files['file']
             if not file:
-                return "No handbag detected\n"
+                return render_template("no_result.html")
 
             if file and allowed_file(file.filename):
                 filename = secure_filename(file.filename)
-                file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
-                #input_name = 'http://127.0.0.1:5000/uploads/' + filename
+                savename = os.path.join(app.config["UPLOAD_FOLDER"], filename)
+                if os.path.exists(savename):
+                    os.remove(savename)
+                file.save(savename)
                 file.stream.seek(0)
                 
         img_bytes = file.read()
@@ -251,7 +380,10 @@ def predict():
         if option == "yes":
             infile = crop_image(img_bytes, detection_model)
             if infile == False:
-                return "No handbag detected\n"
+                return render_template("no_result.html")
+            savename_crop = os.path.join(app.config["UPLOAD_FOLDER"], filename.split(".")[0] + "_crop.jpg")
+            if os.path.exists(savename_crop):
+                os.remove(savename_crop)
             infile.save(os.path.join(app.config["UPLOAD_FOLDER"], filename.split(".")[0] + "_crop.jpg"))
             crop_filename = filename.split(".")[0] + "_crop.jpg"
         else:
@@ -259,23 +391,25 @@ def predict():
             crop_filename = filename
         
         brand, name, web, input_vector = get_prediction(image=infile, model=model, model_vector=model_vector)
-        price_range = request.form.get("price")
-        company = request.form.get("company")
-        Top1, Top2, Top3 = recommend_bag(input_vector, shop_data_saks, shop_data_shein, price_range, vectors_shein, vectors_shop, company)
-        return render_template('result.html', class_brand=brand, class_name=name, class_web=web,
-        Top1_product=Top1[1], Top2_product=Top2[1], Top3_product=Top3[1],
-        Top1_sim=Top1[-1], Top2_sim=Top2[-1], Top3_sim=Top3[-1],
-        Top1_page=Top1[0], Top2_page=Top2[0], Top3_page=Top3[0],
-        Top1_brand=Top1[2], Top2_brand=Top2[2], Top3_brand=Top3[2],
-        Top1_price=Top1[3], Top2_price=Top2[3], Top3_price=Top3[3],
-        Top1_img=Top1[4],Top2_img=Top2[4], Top3_img=Top3[4],
-        filename=filename,
-        crop_filename=crop_filename)
+        if name != "None":
+            price_range = request.form.get("price")
+            company = request.form.get("company")
+            Top1, Top2, Top3 = recommend_bag(input_vector, shop_data_saks, shop_data_shein, price_range, vectors_shein, vectors_shop, company)
+            return render_template('result.html', class_brand=brand, class_name=name, class_web=web,
+            Top1_product=Top1[1], Top2_product=Top2[1], Top3_product=Top3[1],
+            Top1_sim=Top1[-1], Top2_sim=Top2[-1], Top3_sim=Top3[-1],
+            Top1_page=Top1[0], Top2_page=Top2[0], Top3_page=Top3[0],
+            Top1_brand=Top1[2], Top2_brand=Top2[2], Top3_brand=Top3[2],
+            Top1_price=Top1[3], Top2_price=Top2[3], Top3_price=Top3[3],
+            Top1_img=Top1[4],Top2_img=Top2[4], Top3_img=Top3[4],
+            filename=filename,
+            crop_filename=crop_filename)
+        else:
+            return render_template("no_result.html")
     return render_template('index.html')
-
 
 if __name__ == '__main__':
     print("Loading PyTorch model and Flask starting server ...")
     print("Please wait until server has fully started")
     app.run(host="0.0.0.0", port=80, debug=False)
-    ### Command to run this: FLASK_ENV=development FLASK_APP=app.py FLASK_DEBUG=0 
+    ### Command to run this: FLASK_ENV=development FLASK_APP=app.py FLASK_DEBUG=0 flask run
